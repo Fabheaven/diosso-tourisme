34178feaf8e0b9649769ad2c4dcc8b499cf059e7
Fabheaven <fabricemoukengue@gmail.com>
2025-01-15 14:45:56 +0100

    ajout de l'envoi du formulaire par mail avec mailtrap test ok

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -1,19 +1,19 @@
 <?php
-
 namespace App\Controller;
 
 use App\Entity\Contact;
 use App\Form\ContactType;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(Request $request, EntityManagerInterface $manager): Response
+    public function index(Request $request, MailerInterface $mailer): Response
     {
         $contact = new Contact();
 
@@ -29,9 +29,24 @@ class ContactController extends AbstractController
         // Gestion du formulaire
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            // Sauvegarde des données en base de données
-            $manager->persist($contact);
-            $manager->flush();
+            
+            // Envoi de l'email
+            $email = (new TemplatedEmail())
+                ->from($contact->getEmail())
+                ->to('admin@diossotourisme.com')
+                ->subject($contact->getSubject())
+                 // path of the Twig template to render
+                ->htmlTemplate('emails/contact.html.twig')
+
+                // change locale used in the template, e.g. to match user's locale
+                ->locale('de')
+
+                // pass variables (name => value) to the template
+                ->context([
+                    'contact' => $contact,
+                ]);
+
+            $mailer->send($email);
 
             // Message flash pour informer l'utilisateur
             $this->addFlash('success', 'Votre demande a été envoyée avec succès !');
