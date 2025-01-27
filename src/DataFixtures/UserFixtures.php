32b1a63da61811ee4f4d551be8ece374951fafad
Fabheaven <fabricemoukengue@gmail.com>
2025-01-27 12:01:22 +0100

    ajout d'un  user et un mdp par défaut pour test.

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -20,18 +20,33 @@ class UserFixtures extends Fixture
     {
         $faker = Factory::create('fr_FR');
 
+        // Créer un nouvel utilisateur
+        $user = new User();
+        $user->setEmail('fab@exemple.fr')
+             ->setFirstName('Fabrice')
+             ->setLastName('Mouk')
+             ->setPhone($faker->phoneNumber)
+             ->setAddress($faker->address);
+
+        // Définir un mot de passe par défaut et le hasher
+        $defaultPassword = 'Bonjour123'; // Choisissez un mot de passe par défaut approprié
+        $hashedPassword = $this->hasher->hashPassword($user, $defaultPassword);
+        $user->setPassword($hashedPassword);
+
+        $manager->persist($user);
+
         for ($i = 0; $i < 10; $i++) {
             $user = new User();
 
-            // L'UUID est généré automatiquement dans le constructeur de l'entité, donc pas besoin de le définir ici
-
-            // Définir les autres informations de l'utilisateur
+            // Définir les informations de l'utilisateur
             $user->setEmail($faker->email)
                 ->setLastName($faker->lastName)
                 ->setFirstName($faker->firstName)
                 ->setPassword(
                     $this->hasher->hashPassword($user, 'password') // Hash le mot de passe
-                );
+                )
+                ->setPhone($faker->phoneNumber) // Génère un numéro de téléphone aléatoire
+                ->setAddress($faker->address);  // Génère une adresse aléatoire
 
             // Persister l'utilisateur dans la base de données
             $manager->persist($user);
